aecf9aed9ad6ee560718b9dda5ea5525c9e8efc9
Melissa Longenberger <mlongenberger@codingdojo.com>
2023-05-18 13:06:57 -0400

    adding files

---
+++ lectures/week1/Thursday/BreakOutRoomWork/room1.js
@@ -0,0 +1,11 @@
+// Example
+function add(a,b) {
+    var sum = a + b
+    console.log("Sum = ", sum)
+}
+add(3, 4)
+// Line 2 = A Function called add taking 2 parameters called a and b
+// Line 3 = Starting instructions - creating a var called sum and setting it equal to the sum of a and b
+// Line 4 = Printing to the console the value of sum with the comment of Sum = before it
+// Line 5 = Indicates end of instructions and in this case the function a well
+// Line 6 = Calling or instantiating the function called add and providing the values for the variables needed for the function of 3 for a and 4 for b
