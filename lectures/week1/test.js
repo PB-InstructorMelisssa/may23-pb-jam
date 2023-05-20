b70d950029f4d63d91395b8c89ea88a4421f46b5
Melissa Longenberger <mlongenberger@codingdojo.com>
2023-05-20 14:55:30 -0400

    saving extra file

---
+++ lectures/week1/test.js
@@ -0,0 +1,22 @@
+var min = 42
+var age = 10
+var rider01 = [44, 12]
+var rider02 = [44, 9]
+var rider03 = [30, 12]
+var rider04 = [30, 9]
+
+if(rider02[0] > min  && rider02[1] > age) {
+    console.log('me good')
+} else {
+    console.log('me no good')
+}
+
+// line 8 = if riderXX[1st value] is greater than the value of min AND riderXX[2nd value] is greater than the value of age
+// line 9 =  print to console me good
+// line 10 = otherwise
+// line 11 = print to console me no good
+
+
+for(i = 0; i < 10; i++) {
+    console.log(i)
+}
