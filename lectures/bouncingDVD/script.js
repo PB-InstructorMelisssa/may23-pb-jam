0dfeadfd0e801bf3827f41ea9989c31809a6f8c3
Melissa Longenberger <mlongenberger@codingdojo.com>
2023-05-24 16:42:04 -0400

    saving

---
+++ lectures/bouncingDVD/script.js
@@ -0,0 +1,9 @@
+console.log('js file connected')
+
+var node = document.createElement('img')
+node.src = './404Error.png'
+node.alt = '404 Error'
+// node.setAttribute = ('class', 'logo')
+const logo = document.getElementById('dvd').appendChild(node)
+logo.className = 'logo'
+
